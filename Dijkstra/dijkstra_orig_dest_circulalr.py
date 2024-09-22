e6ad9dd4ad0864fa5313f2f2ccffe1399e0d3931
Ed Scrimaglia <edgardo.scrimaglia@gmail.com>
2024-09-22 13:00:47 -0300

    refactoring tipo de grafo

--- Dijkstra/dijkstra_orig_dest_circulalr.py
+++ Dijkstra/dijkstra_orig_dest_circulalr.py
@@ -9,6 +9,7 @@ import heapq
 import networkx as nx
 import matplotlib.pyplot as plt
 from typing import Any, Tuple
+from networkx.drawing.nx_agraph import graphviz_layout
 
 class Grafo:
     def __init__(self, dirigido: bool = True):
@@ -60,18 +61,19 @@ class Grafo:
             nodo_actual = predecesores[nodo_actual]
         return camino
 
-    # Graficar grafo by IA
-    def graficar_grafo(self, distancia_total: int, camino_resaltado: list = []):
+    # Graficar grafo tipo circular (IA)
+    def graficar_grafo_circular(self, distancia_total: int, camino_resaltado: list = []):
         G = nx.DiGraph() if self.dirigido else nx.Graph()
 
         for origen in self.grafo:
             for destino, costo in self.grafo[origen]:
                 G.add_edge(origen, destino, weight=costo)
 
-        # Circular para garantizar distancias equidistantes
+        # Layout Circular
         pos = nx.circular_layout(G)
 
-        plt.figure(figsize=(10, 8))
+        # Tamaño del gráfico
+        plt.figure(figsize=(13, 8))
         nx.draw(G, pos, with_labels=True, node_size=500, node_color='lightblue', font_size=12, font_weight='bold', edge_color='gray', width=2)
         
         edge_labels = nx.get_edge_attributes(G, 'weight')
@@ -81,6 +83,8 @@ class Grafo:
             camino_edges = [(camino_resaltado[i], camino_resaltado[i+1]) for i in range(len(camino_resaltado) - 1)]
             nx.draw_networkx_edges(G, pos, edgelist=camino_edges, edge_color='blue', width=2)
             nx.draw_networkx_nodes(G, pos, nodelist=camino_resaltado, node_color='yellow', node_size=600)
+            camino_edge_labels = {edge: edge_labels[edge] for edge in camino_edges if edge in edge_labels}
+            nx.draw_networkx_edge_labels(G, pos, edge_labels=camino_edge_labels, font_color='blue', font_size=12)
 
         plt.text(
             0.5, 0.05, f"Menor distancia: {distancia_total}", 
@@ -89,13 +93,51 @@ class Grafo:
             transform=plt.gca().transAxes
         )
 
-        plt.title("Grafo con el camino más corto resaltado", fontsize=16, fontweight='bold')
+        plt.title("Grafo con el camino más corto resaltado", fontsize=12, fontweight='bold')
+        plt.show()
+
+    # Graficar grafo tipo Arbol (IA)
+    def graficar_grafo_arbol(self, distancia_total: int, camino_resaltado: list = []):
+        G = nx.DiGraph() if self.dirigido else nx.Graph()
+
+        for origen in self.grafo:
+            for destino, costo in self.grafo[origen]:
+                G.add_edge(origen, destino, weight=costo)
+
+        # Layout jerárquico
+        pos = graphviz_layout(G, prog='dot')
+
+        # Tamaño del gráfico
+        plt.figure(figsize=(13, 8))
+
+        nx.draw(G, pos, with_labels=True, node_size=500, node_color='lightblue', font_size=12, font_weight='bold', edge_color='gray', width=2)
+        
+        edge_labels = nx.get_edge_attributes(G, 'weight')
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color='red', font_size=12)
+
+        if camino_resaltado:
+            camino_edges = [(camino_resaltado[i], camino_resaltado[i+1]) for i in range(len(camino_resaltado) - 1)]
+            nx.draw_networkx_edges(G, pos, edgelist=camino_edges, edge_color='blue', width=2)
+            nx.draw_networkx_nodes(G, pos, nodelist=camino_resaltado, node_color='yellow', node_size=600)
+            camino_edge_labels = {edge: edge_labels[edge] for edge in camino_edges if edge in edge_labels}
+            nx.draw_networkx_edge_labels(G, pos, edge_labels=camino_edge_labels, font_color='blue', font_size=12)
+
+
+        plt.text(
+            0.5, 0.05, f"Menor distancia: {distancia_total}", 
+            horizontalalignment='center', verticalalignment='center', 
+            fontsize=14, fontweight='bold', color='black',
+            transform=plt.gca().transAxes
+        )
+
+        plt.title("Grafo en formato jerárquico con el camino más corto resaltado", fontsize=12, fontweight='bold')
         plt.show()
 
 if __name__ == "__main__":
     dirigido = True 
-    grafo = Grafo(dirigido)
+    tipo_grafico = "arbol"
 
+    grafo = Grafo(dirigido)
     grafo.agregar_arista("CO", "FR", 565)
     grafo.agregar_arista("CO", "VM",702)
     grafo.agregar_arista("CO", "R4", 210)
@@ -128,7 +170,12 @@ if __name__ == "__main__":
     if distancia < float('inf'):
         camino = grafo.reconstruir_camino(predecesores, nodo_final)
         print (f"El camino más corto desde {nodo_inicial} hasta {nodo_final} es: {camino}")
-        grafo.graficar_grafo(distancia, camino_resaltado=camino)
+        if tipo_grafico.lower() == "circular":
+            grafo.graficar_grafo_circular(distancia, camino_resaltado=camino)
+        elif tipo_grafico.lower() == "arbol":
+            grafo.graficar_grafo_arbol(distancia, camino_resaltado=camino)
+        else:
+            print ("Tipo de gráfico debe ser Circular o Arbol")
     else:
         print (f"No hay camino desde el nodo {nodo_inicial} al nodo {nodo_final}")
         grafo.graficar_grafo(distancia)
--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ## Dijkstra: resolución de problemas de camino mas corto (shortest path)
 
+### El gráfico puede ser de tipo circular o jerarquico (arbol)
+
 ### dijkstra_orig_to_allnodes: resuelve el shortest path entre el nodo original y cada nodo del grafo. El grafo puede ser dirigido o no dirigido. Se selecciona nodo origen
 
 ### dijkstra_orig_dest: resuelve el shortest path entre el nodo original y el nodo final. Se seleccionan ambos nodos
